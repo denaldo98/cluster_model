941434216ac2fa3989c82397f358d1b8cf2765cb
Denaldo Lapi <55629884+denaldo98@users.noreply.github.com>
2023-01-03 11:19:40 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='cluster_model',
+    version='0.0.1',
+    author='Denaldo Lapi',
+    author_email='denaldo98@gmail.com',
+    description='Class for wrapping River cluster model for BERTopic Online Topic Modeling',
+    long_description='',
+    long_description_content_type="text/markdown",
+    url='https://github.com/denaldo98/cluster_model',
+    license='MIT',
+    packages=['river_class'],
+    install_requires=['river'],
+)
